9840de1a5e2b45e8ddc3c1a0a97bf3ecea075195
WillyWilliam00 <133374335+WillyWilliam00@users.noreply.github.com>
2023-10-05 15:59:24 +0000

    up

--- AddProduct/Addproduct.js
+++ AddProduct/Addproduct.js
@@ -4,26 +4,24 @@ const DescriptionProduct = document.querySelector("#description")
 const BrandProduct = document.querySelector("#brand")
 const ImageProduct = document.querySelector("#image")
 const PriceProduct = document.querySelector("#price")
+const PrevieWbutton = document.querySelector(".PreviewButton")
 
-function  ShowModal(){
-  let myModal = new bootstrap.Modal(document.getElementById('myModal'), {});
-  myModal.show();
-}
 
 
 function preview() {
-  
-  
+
+  FocusInputWithoutValue()
   if ((NameProduct.value !== "") && (DescriptionProduct.value !== "") && (BrandProduct.value !== "") && (ImageProduct.value !== "") && (PriceProduct.value !== "")) {
     
     prew.innerHTML = `
-    
-    
-    
        <div class="card mb-3 border-0">
           <div class="row g-0 my-auto border-radius-black">
               <div class="col-6">
-                <img src="${ImageProduct.value}" class="card-img-top border-radius" alt="..." style="height: 100%; aspect-ratio: 0.8; object-fit: cover;">
+              <a href="" class="position-relative">
+                 <img src="${ImageProduct.value}" class="card-img-top border-radius-img" alt="..." style="height: 100%; aspect-ratio: 0.8; object-fit: cover;">
+                  <i class="bi bi-plus-circle more fs-2"></i>
+              </a>
+                
               </div>
               <div class="col-6">
                   <div class="card-body d-flex flex-column" style= "height: 100%;">
@@ -32,8 +30,10 @@ function preview() {
                           <p class="card-text py-3 m-0 border-bottom border-secondary-subtle"><span class="fw-bolder">Prezzo:  </span>${PriceProduct.value}€</p>
                           <p class="card-text py-3 m-0 border-bottom border-secondary-subtle"><span class="fw-bolder">Brand:  </span>${BrandProduct.value}</p>
                       </div> 
-                      <span class="fw-bolder">Description:</span>
-                      <p class="card-text my-auto pt-2" style= "max-height: 200px; overflow: auto;">${DescriptionProduct.value}</p>
+                      <span class="fw-bolder py-3">Description:</span>
+                      <p class="card-text my-auto mb-2 border overflow-auto" style= "max-height: 130px;">${DescriptionProduct.value}</p>
+                      <div class="pt-2"><button type="button" class="border-0 btn btn-primary bg-success border-0 fs-5 text-light"><span><i class="bi bi-cart cart-main"></i></span>
+                       </button></div>
                   </div>
               </div>
            </div>
@@ -45,18 +45,9 @@ function preview() {
 }
 
 
-
-function CleanForm() {
-  
-  for (const input of [NameProduct, DescriptionProduct, BrandProduct, ImageProduct, PriceProduct]) {
-    input.value = ''
-  }
-
-}
-
 async function AddProduct(event) {
   event.preventDefault()
-
+  
   const response = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
     method: "POST",
     headers: {
@@ -75,13 +66,7 @@ async function AddProduct(event) {
 
   if (response.ok) {
     ShowModal()
-    prew.innerHTML = ""
-    
-
-
-    for (const input of [NameProduct, DescriptionProduct, BrandProduct, ImageProduct, PriceProduct]) {
-      input.value = ''
-    }
+    CleanForm()
   } else {
     console.error("Cannot send")
   }
@@ -89,3 +74,32 @@ async function AddProduct(event) {
 
 
 
+function  ShowModal(){
+  let myModal = new bootstrap.Modal(document.getElementById('myModal'), {});
+  myModal.show();
+}
+
+document.addEventListener("click",  function (event) {
+  if(event.target !== PrevieWbutton) {
+
+    for (const input of [NameProduct, DescriptionProduct, BrandProduct, ImageProduct, PriceProduct]) {
+       input.classList.remove("focus-input")
+       
+   }
+  }
+})
+
+function CleanForm() {
+  prew.innerHTML = ""
+  for (const input of [NameProduct, DescriptionProduct, BrandProduct, ImageProduct, PriceProduct]) {
+    input.value = ''
+  }
+
+}
+function FocusInputWithoutValue() {
+  prew.innerHTML = `<h2 class="text-danger">Compila tutti i campi per procedere!</h2>`
+  for (const input of [NameProduct, DescriptionProduct, BrandProduct, ImageProduct, PriceProduct]) {
+    if(input.value === "") return input.classList.add("focus-input")
+    
+ }
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Row = document.querySelector(".row")
 
 
+
 async function GetProducts() {
     const response = await fetch("https://striveschool-api.herokuapp.com/api/product/",
                                 { headers: { "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTFjZjIwYzUyYmJmMzAwMTg3OWIxZTgiLCJpYXQiOjE2OTYzOTU3ODgsImV4cCI6MTY5NzYwNTM4OH0.9R7rOYsAE9jENc32hvt3ua7fc2bv2dWkSDK-PGXuOFE" }}  )
@@ -8,35 +9,61 @@ async function GetProducts() {
     console.log(data)
     return data
 }
- function DisplayProducts(data) {
+function text() { //funzione che aggiorna il dom in base all'input value search
+    
+    const value = document.querySelector("input").value
+        let ArrayProductSearch = result.filter(product => 
+            product.toLowerCase().includes(value.toLowerCase())
+        )
+        
+        DisplayProducts(ArrayProductSearch)
+}
 
+    
+        
+    
+window.onload = async () => {
+    Row.innerHTML = /*html*/ 
+
+    `<div class="lds-ring"><div></div><div></div><div></div><div></div></div>`
+
+    try {
+        const data = await GetProducts()
+        DisplayProducts(data);
+      } catch(err) {
+        alert("oh oh");
+      } finally {
+        Row.querySelector(".lds-ring").remove();
+      }
+}
+
+function DisplayProducts(data) {
+    
     Row.innerHTML = data.map(product => /*html*/ 
         
-     `<div class="col-6">
-        <div class="card mb-3 border-0">
-          <div class="row g-0 my-auto border-radius-black">
-              <div class="col-6">
-                <img src="${product.imageUrl}" class="card-img-top border-radius" alt="..." style="height: 100%; aspect-ratio: 0.8; object-fit: cover;">
-              </div>
-              <div class="col-6">
-                  <div class="card-body d-flex flex-column" style= "height: 100%;">
-                      <div>
-                          <h5 class="card-title card-title-inbody pb-3 m-0 border-bottom border-secondary-subtle">${product.name}</h5>
-                          <p class="card-text py-3 m-0 border-bottom border-secondary-subtle"><span class="fw-bolder">Prezzo:  </span>${product.price}€</p>
-                          <p class="card-text py-3 m-0 border-bottom border-secondary-subtle"><span class="fw-bolder">Brand:  </span>${product.brand}</p>
-                      </div> 
-                      <span class="fw-bolder">Description:</span>
-                      <p class="card-text my-auto pt-2" style= "max-height: 200px; overflow: auto;">${product.description}</p>
-                  </div>
-              </div>
-           </div>
-           </div>  
-        </div>`
+     `<div class="card mb-3 border-0">
+     <div class="row g-0 my-auto border-radius-black">
+         <div class="col-6">
+         <a href="" class="position-relative">
+            <img src="${product.value}" class="card-img-top border-radius-img" alt="..." style="height: 100%; aspect-ratio: 0.8; object-fit: cover;">
+             <i class="bi bi-plus-circle more fs-2"></i>
+         </a>
+           
+         </div>
+         <div class="col-6">
+             <div class="card-body d-flex flex-column" style= "height: 100%;">
+                 <div>
+                     <h5 class="card-title card-title-inbody pb-3 m-0 border-bottom border-secondary-subtle">${product.value}</h5>
+                     <p class="card-text py-3 m-0 border-bottom border-secondary-subtle"><span class="fw-bolder">Prezzo:  </span>${product.value}€</p>
+                     <p class="card-text py-3 m-0 border-bottom border-secondary-subtle"><span class="fw-bolder">Brand:  </span>${product.value}</p>
+                 </div> 
+                 <span class="fw-bolder py-3">Description:</span>
+                 <p class="card-text my-auto mb-2 border overflow-auto" style= "max-height: 130px;">${product.value}</p>
+                 <div class="pt-2"><button type="button" class="border-0 btn btn-primary bg-success border-0 fs-5 text-light"><span><i class="bi bi-cart cart-main"></i></span>
+                  </button></div>
+             </div>
+         </div>
+      </div>
+   </div>`
     ).join("")
     }
-
-
-window.onload = async () => {
-    const data = await GetProducts()
-    DisplayProducts(data)
-}
